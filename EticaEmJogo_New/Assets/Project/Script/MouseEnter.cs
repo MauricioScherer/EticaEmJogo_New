8433e30822cabff952b8da0f57d8ecf3f72d3f6c
MauricioScherer <mauricio.sscherer@gmail.com>
2017-06-21 01:55:35 -0300

    AssetOffice

---
+++ EticaEmJogo_New/Assets/Project/Script/MouseEnter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEnter : MonoBehaviour
+{
+    public Texture2D eyeCursor;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(eyeCursor, Vector2.zero, CursorMode.Auto);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+}
