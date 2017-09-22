8160f9267d9b7dc6f30c31d5df3258111cd40c95
MauricioScherer <mauricio.sscherer@gmail.com>
2017-09-22 17:05:57 -0300

    InitialLevel5

---
+++ EticaEmJogo_New/Assets/Project/Script/Level5/BallonDialogueBigBoss.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallonDialogueBigBoss : MonoBehaviour
+{
+    public GameObject[] textDialogue;
+    public GameObject npc;
+    public TextMesh ballon1;
+
+    void Start()
+    {
+        ballon1.text = "Olá " + PlayerPrefs.GetString("nameSelect") + "!" + "\n Você teve uma jornada \n e tanto até aqui em!";
+    }
+
+    public void NextDialogue()
+    {
+        for (int i = 0; i < textDialogue.Length; i++)
+        {
+            if (i < textDialogue.Length - 1)
+            {
+                if (textDialogue[i].activeSelf)
+                {
+                    textDialogue[i].SetActive(false);
+                    textDialogue[i + 1].SetActive(true);
+                    if (!npc.GetComponent<AudioSource>().isPlaying)
+                        npc.GetComponent<AudioSource>().Play();
+                    break;
+                }
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ EticaEmJogo_New/Assets/Project/Script/Level5/ManagerPlayerLV5.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerPlayerLV5 : MonoBehaviour
+{
+    public GameObject[] poolAvatar;
+
+    void Awake()
+    {
+        if (Time.deltaTime < 1)
+            Time.timeScale = 1;
+        StartAvatar();
+    }
+
+    public void StartAvatar()
+    {
+        if (PlayerPrefs.HasKey("avatarSelect"))
+        {
+            int __tempNumberPool = PlayerPrefs.GetInt("avatarSelect");
+            poolAvatar[__tempNumberPool].SetActive(true);
+        }
+        else
+        {
+            poolAvatar[0].SetActive(true);
+        }
+    }
+
+    public void LoadNewScene()
+    {
+        SceneManager.LoadScene("Scene_02");
+    }
+}
